52cb636928a3413208da44d4fad4de4f06842aa8
Hanib2404 <yusufibro24@gmail.com>
2020-08-29 17:34:11 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -5,14 +5,34 @@ var typed = new Typed('.typed', {
     loop:true,
 });
 
-var offsetTop = $('#sekils').offset().top;
-$(window).scroll(function() {
-var height = $(window).height();
-if($(window).scrollTop()+height > offsetTop) {
-jQuery('.skillbar').each(function(){
-  jQuery(this).find('.skillbar-bar').animate({
-    width:jQuery(this).attr('data-percent')
-  },2000);
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},4000);
+	});
 });
-}
-});
+
+jQuery('.Count').each(function () {
+    var $this = $(this);
+    jQuery({ Counter: 0}).animate({ Counter: $this.text() }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function () {
+        $this.text(Math.ceil(this.Counter));
+      }
+    });
+  });
+
+// var offsetTop = $('#sekils').offset().top;
+//     $(window).scroll(function() {
+//         var height = $(window).height();
+//         if($(window).scrollTop()+height > offsetTop) {
+//             jQuery('.skillbar').each(function(){
+//                 jQuery(this).find('.skillbar-bar').animate({
+//                     width:jQuery(this).attr('data-percent')
+//                 },2000);
+//             });
+//         }   
+// });
